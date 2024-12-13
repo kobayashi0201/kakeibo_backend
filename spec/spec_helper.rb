326a6a03da65eb2fcb05db9fd76798e09ccd83d2
kobayashi0201 <kota0201bako@gmail.com>
2024-12-13 18:57:57 +0900

    simplecov導入

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -92,3 +94,8 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+SimpleCov.start 'rails' do
+  add_filter '/spec/'
+end
+SimpleCov.coverage_dir('coverage')
